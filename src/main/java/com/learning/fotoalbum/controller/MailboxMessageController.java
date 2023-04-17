bbe1781742899044892cb14d9e6365ff52bd7a27
Mirko Robbi <mirko.robbi@gmail.com>
2023-04-17 14:29:48 +0200

    PUSH api and mailbox

--- src/main/java/com/learning/fotoalbum/controller/MailboxMessageController.java
+++ src/main/java/com/learning/fotoalbum/controller/MailboxMessageController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.server.ResponseStatusException;
 
+
+
 @Controller
 @RequestMapping("/mailbox")
 public class MailboxMessageController {
