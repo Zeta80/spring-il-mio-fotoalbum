5f1899815a6bcd96e4e687fe6552344da6f9adce
Mirko Robbi <mirko.robbi@gmail.com>
2023-04-17 14:54:24 +0200

    ADJUST

--- src/main/java/org/lessons/springilmiofotoalbum/exeption/PhotoNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package org.lessons.springilmiofotoalbum.exeption;
-
-public class PhotoNotFoundException extends RuntimeException{
-        public PhotoNotFoundException(String message) {
-            super(message);
-        }
-    }
-
